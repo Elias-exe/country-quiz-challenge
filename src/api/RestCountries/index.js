738a6d45c2f36cd08f6d0cc80aa6bfeed167609d
Machado Gabriel <Gabriel.Machado@andritz.com>
2022-12-19 14:15:33 -0300

    feat: Reducing the number of renders

--- src/api/RestCountries/index.js
+++ src/api/RestCountries/index.js
@@ -4,14 +4,22 @@ import { useEffect, useState } from 'react';
 export default function RestCountries() {
   const [data, setData] = useState({});
 
+  async function handleData() {
+    try {
+      await axios({
+        method: 'get',
+        url: 'https://restcountries.com/v2/name/Brazil',
+      })
+        .then((response) => {
+          setData(response);
+        });
+    } catch {
+      console.log('Algo deu errado');
+    }
+  }
+
   useEffect(() => {
-    axios({
-      method: 'get',
-      url: 'https://restcountries.com/v2/name/Brazil',
-    })
-      .then((response) => {
-        setData(response);
-      });
+    handleData();
   }, []);
   return {
     data,
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,22 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+import { useEffect, useState } from 'react';
 import { Container } from './styles';
 
 import RestCountries from '../../api/RestCountries';
 
 export default function Card() {
   const { data } = RestCountries();
+  const [capitalFiltered, setCapitalFiltered] = useState();
 
-  console.log(data);
+  useEffect(() => {
+    data.data?.map((capital) => (
+      setCapitalFiltered(capital.capital)
+    ));
+  }, [data]);
 
   return (
     <Container>
-      <h1 className="quizQuestion">Kuala Lumpur is the capital of</h1>
+      <h1 className="quizQuestion">{capitalFiltered} is the capital of</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit.
         Nihil quas necessitatibus nulla, et magni praesentium possimus cupiditate
